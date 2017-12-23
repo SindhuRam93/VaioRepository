41d7a4f6f9e45788e704e4fecdb217f6d8f21391
Sreeram <Sreeram@LAPTOP-0NL3KTLK>
2017-12-23 23:27:26 +0530

    Upload WebDriverWait

---
+++ Selenium-Nov/src/main/java/selenium/learning/WebDriverWaitDemo.java
@@ -0,0 +1,39 @@
+package selenium.learning;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebDriverWaitDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "F:/Selenium/drivers/chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+
+		// enter the URL
+		driver.get("https://www.flipkart.com/");
+		//maximize the window
+		driver.manage().window().maximize();
+		//mouseover electronics
+		
+		 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		WebElement ele =driver.findElementByXPath("//span[text()='Electronics']");
+		//mouse overing using actions class
+		Actions builder = new Actions(driver);
+		builder.moveToElement(ele).perform();
+		
+		//dynamic wait
+		WebDriverWait wait =new WebDriverWait(driver, 10);
+		WebElement ele2 =driver.findElementByXPath("//span[text()='Apple']");
+		wait.until(ExpectedConditions.elementToBeClickable(ele2));
+		ele2.click();
+	}
+
+}
